25ce9edfbd9cb197230966449537aab415215270
ggc1509 <ggc1509@gmail.com>
2026-09-14 20:09:31 -0600

    Crear Actividad 2/Act2.py

---
+++ Semestre 1/Modelos estadisticos/Unidad 1/Actividad 2/Act2.py
@@ -0,0 +1,11 @@
+import pandas as pd
+# Crear DataFrame vacío con la estructura del Weather Dataset
+weather_data = {
+ 'outlook': ['sunny', 'sunny', 'overcast', 'rainy', 'rainy'],
+ 'temperature': ['hot', 'hot', 'hot', 'mild', 'cool'],
+ 'humidity': ['high', 'high', 'high', 'high', 'normal'],
+ 'windy': [False, True, False, False, False],
+ 'play': ['no', 'no', 'yes', 'yes', 'yes']
+}
+df_weather = pd.DataFrame(weather_data)
+print(df_weather.head())
